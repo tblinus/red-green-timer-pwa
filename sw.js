d0c9fa338b49337af98f30de72017ec4a8c41bb1
tblinus <tblinus@hotmail.com>
2025-10-17 20:39:00 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,43 +1,13 @@
-// Simple service worker for Red/Green Timer PWA
-const CACHE_NAME = 'rg-timer-cache-v1';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.webmanifest',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('rg-timer-v1').then(cache => cache.addAll([
+      'index.html',
+      'manifest.webmanifest',
+      'icons/icon-192.png',
+      'icons/icon-512.png'
+    ]))
   );
-  self.skipWaiting();
 });
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.map(k => k !== CACHE_NAME && caches.delete(k))
-    ))
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', event => {
-  const { request } = event;
-  // Network-first for HTML, cache-first for others
-  if (request.mode === 'navigate' || (request.headers.get('accept') || '').includes('text/html')) {
-    event.respondWith(
-      fetch(request).then(resp => {
-        const copy = resp.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put(request, copy));
-        return resp;
-      }).catch(() => caches.match('./index.html'))
-    );
-  } else {
-    event.respondWith(
-      caches.match(request).then(cached => cached || fetch(request))
-    );
-  }
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
