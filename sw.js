086ac9160012c71f9df97f625e1a9bdaa76a0361
tblinus <tblinus@hotmail.com>
2025-10-19 08:04:31 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,13 +1,3 @@
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open('rg-timer-v1').then(cache => cache.addAll([
-      'index.html',
-      'manifest.webmanifest',
-      'icons/icon-192.png',
-      'icons/icon-512.png'
-    ]))
-  );
-});
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
-});
+self.addEventListener('install', e => { self.skipWaiting(); });
+self.addEventListener('activate', e => { self.clients.claim(); });
+self.addEventListener('fetch', e => { e.respondWith(fetch(e.request)); });
