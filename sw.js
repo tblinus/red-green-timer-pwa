91f1b9aeca5c99912b9e20d270d56f0690834d02
tblinus <tblinus@hotmail.com>
2025-10-19 07:01:51 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,21 +1,2 @@
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open('timer-cache-v1').then((cache) => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/manifest.webmanifest',
-        '/icons/icon-192.png',
-        '/icons/icon-512.png'
-      ]);
-    })
-  );
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
-});
+self.addEventListener('install', e => { self.skipWaiting(); });
+self.addEventListener('fetch', e => { e.respondWith(fetch(e.request)); });
